24d6336ee6d9eeac4afb685e568810577a554e3c
creatorQuen <spastime@yandex.ru>
2021-08-29 21:15:11 +0300

    fix start and end rows

--- Insertner2000/ConfigurationForTables.cs
+++ Insertner2000/ConfigurationForTables.cs
@@ -10,24 +10,25 @@ namespace Insertner2000
         //public const string _conStringCRM = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LeadA; Persist Security Info=False;";
         public const string _conStringCRM = @"Data Source=80.78.240.16;Initial Catalog = CRM.Db; Persist Security Info=True;User ID = student;Password=qwe!23;";
         public const string _conStringTStore = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TStore; Persist Security Info=False;";
-        public const int _countRows = 1000000;
+        public const int _startCountRows = 1;
+        public const int _endCountRows = 1000000;
 
         public void LeadsDataBase()
         {
             Lead listLeads = new Lead();
-            listLeads.CreateLeads(1, _countRows, _conStringCRM);
+            listLeads.CreateLeads(_startCountRows, _endCountRows, _conStringCRM);
         }
 
         public void CitiesDataBase()
         {
             City listCities = new City();
-            listCities.CreateCities(1, _conStringCRM);
+            listCities.CreateCities(_startCountRows, _conStringCRM);
         }
 
         public void AccountsDataBase()
         {
             Account listAccounts = new Account();
-            listAccounts.CreateAcounts(1, _countRows, _conStringCRM, _conStringTStore);
+            listAccounts.CreateAcounts(_startCountRows, _endCountRows, _conStringCRM, _conStringTStore);
         }
     }
 }
