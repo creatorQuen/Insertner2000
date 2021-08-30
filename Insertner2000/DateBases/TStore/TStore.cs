c736afaf77c1e4b8e3dc9e1163d60fbc93809747
creatorQuen <spastime@yandex.ru>
2021-08-30 03:03:34 +0300

    work version 1

--- Insertner2000/DateBases/TStore/TStore.cs
+++ Insertner2000/DateBases/TStore/TStore.cs
@@ -35,11 +35,40 @@ namespace Insertner2000.DateBases.TStore
                 {
 
                     // var listId = Enumerable.ToList(dictionary.Keys);
-                    var transactionType = GetTransactionTypeByAmount(ammount);
+                    var transactionType = GetTransactionTypeByAmount(ammount, dictionary);
                     var currency = GetCurrencyByDictionaryKeys(dictionary);
                     var dateTimeTransaction = DateTime.Now.AddMilliseconds(ammount).ToString(_dateFormat);
                     var key = dictionary.FirstOrDefault(x => x.Value == currency).Key;
                     ammount = GetRandomAmountByTransactionType(transactionType);
+
+                    if (TransactionType.Transfer == transactionType)
+                    {
+                        var dictionaryClone = new Dictionary<int, CurrencyType>();
+                        foreach (var dic in dictionary)
+                        {
+                            dictionaryClone.Add(dic.Key, dic.Value);
+                        }
+
+                        dictionaryClone.Remove(key);
+
+                        //var payee = GetCurrencyByDictionaryKeys(dictionaryClone);
+
+                        var listId = Enumerable.ToList(dictionaryClone.Keys);
+                        var index = _random.Next(listId[0], listId[listId.Count - 1]);
+                        var payee  = dictionaryClone.FirstOrDefault(x => x.Key == index).Value;
+
+                        var id = dictionaryClone.FirstOrDefault(x => x.Value == payee).Key;
+                        table.Rows.Add(
+                            intRow,
+                            index,//dictionary.key
+                            -ammount,
+                            payee,//dictionary.value
+                            transactionType,
+                            dateTimeTransaction
+                            );
+
+
+                    }
 
                     table.Rows.Add(
                         intRow,
@@ -60,39 +89,50 @@ namespace Insertner2000.DateBases.TStore
             }
         }
 
-        private TransactionType GetTransactionTypeByAmount(int amount)
+        private TransactionType GetTransactionTypeByAmount(int amount, Dictionary<int, CurrencyType> dictionary)
         {
-            //foreach (var dic in Dictionary.Value)
-            //{
-            //if( 1 < Dictionary.Count && 0 < dic)
+            if ( 1 < dictionary.Count)
+            {
+                if (amount == 0)
+                {
+                    return TransactionType.Deposit;
+                }
+
+                if (0 < amount)
+                {
+                    var qqq = GetBoolRandom();
+                    return qqq == true ? TransactionType.Transfer : TransactionType.Deposit;
+                }
+
+                return TransactionType.Withdraw;
+            }
+
             if (0 < amount)
             {
-                return (TransactionType)_random.Next(2, 4);
+                return (TransactionType)_random.Next(1, 3);
             }
-            //}
-
             return TransactionType.Deposit;
         }
 
-        private CurrencyType GetCurrencyByDictionaryKeys(Dictionary<int,CurrencyType> dictionary)
+        private CurrencyType GetCurrencyByDictionaryKeys(Dictionary<int, CurrencyType> dictionary)
         {
-            //var listId = Enumerable.ToList(dictionary.Keys);
-            //var index = _random.Next(listId.Count);
-            //var currency = dictionary.FirstOrDefault(x => x.Key == index).Value;
-            //return currency;
+            var listId = Enumerable.ToList(dictionary.Keys);
+            var index = _random.Next(listId[0], listId[listId.Count - 1]);
+            var currency = dictionary.FirstOrDefault(x => x.Key == index).Value;
+            return currency;
 
             //CurrencyType one = dictionary[0];
             //CurrencyType last = dictionary[dictionary.Count - 1];
 
-            int i = 0;
-            var dicClone = new Dictionary<int, CurrencyType>();
-            foreach (var d in dictionary)
-            {
-                dicClone.Add(i++, d.Value);
-            }
-            var index = _random.Next(dicClone.Count);
-            var currency = dicClone.FirstOrDefault(x => x.Key == index).Value;
-            return currency;
+            //int i = 0;
+            //var dicClone = new Dictionary<int, CurrencyType>();
+            //foreach (var d in dictionary)
+            //{
+            //    dicClone.Add(i++, d.Value);
+            //}
+            //var index = _random.Next(dicClone.Count);
+            //var currency = dicClone.FirstOrDefault(x => x.Key == index).Value;
+            //return currency;
 
 
         }
@@ -110,6 +150,11 @@ namespace Insertner2000.DateBases.TStore
                 case TransactionType.Transfer: return _random.Next(100, 1000);
                 default: throw new Exception("this type doesn't have in TransactionType");
             }
+        }
+
+        private bool GetBoolRandom()
+        {
+            return _random.Next(0, 2) == 1;
         }
     }
 }
